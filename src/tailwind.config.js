538eb08bc8e134dee7c5a215fbf8596fac5aa33c
Zack Hatlen <zckhtln@icloud.com>
2023-01-27 11:53:57 +0300

    master

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -211,20 +211,18 @@ module.exports = ({ nuxt }) => ({
       }
     )
   ],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'content/**/*.md',
-      path.join(nuxt.options.rootDir, 'components/**/*.vue'),
-      path.join(__dirname, 'components/**/*.vue'),
-      path.join(__dirname, 'layouts/**/*.vue'),
-      path.join(__dirname, 'pages/**/*.vue'),
-      path.join(__dirname, 'plugins/**/*.js'),
-      'nuxt.config.js'
-    ],
-    options: {
-      whitelist: ['dark-mode']
-    }
+  // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+  enabled: process.env.NODE_ENV === 'production',
+  content: [
+    'content/**/*.md',
+    path.join(nuxt.options.rootDir, 'components/**/*.vue'),
+    path.join(__dirname, 'components/**/*.vue'),
+    path.join(__dirname, 'layouts/**/*.vue'),
+    path.join(__dirname, 'pages/**/*.vue'),
+    path.join(__dirname, 'plugins/**/*.js'),
+    'nuxt.config.js'
+  ],
+  options: {
+    whitelist: ['dark-mode']
   }
 })
